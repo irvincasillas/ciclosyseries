06ae62a2ebbece2f2b68bf66da0939b589d91cd7
irvincasillas <31043306+irvincasillas@users.noreply.github.com>
2017-09-09 17:32:57 -0500

    Update ciclosyseries.cpp

--- ciclosyseries.cpp
+++ ciclosyseries.cpp
@@ -2,96 +2,87 @@
 #include<cmath>
 
 using namespace std;
-int factorial(int n);
-void seno(int lim, int x);
-void coseno(int lim, int x);
-void logaritmo(int lim, int x);
+void menu();
+float factorial(float n);
+void seno(int x);
+void coseno(int x);
+void logaritmo(int x);
 
 int main()
 {
-	int n,lim;
-	float x;
-	do
-	{
-		do
-		{
-			cout<<"1. Seno\n";
-			cout<<"2. Coseno\n";
-			cout<<"3. Logaritmo Natural\n";
-			cout<<"0. Salir\n";
-			cout<<"Opcion:";
-			cin>>n;
-			
-		}while((n<0)||(n>3));
-		
-		if(n==0)
-			cout<<"Bye bye";
-		else
-		{
-			cout<<"Limite=";
-			cin>>lim;
-			cout<<"x=";
+	int n, x;
+	do{
+		menu();
+		cin >> n;
+		if (n==1){
+			cout<<"Ingresa tamaño";
 			cin>>x;
-			if(n==1)
-				seno(lim,x);
-			else
-				if(n==2)
-					coseno(lim,x);
-				else
-					logaritmo(lim,x);
+			seno(x);
 		}
-		cout<<"\n \n";
+		else if (n==2){
+			cout<<"Ingresa tamaño";
+			cin>>x;
+			coseno(x);
+		}
+		else if (n==3){
+			cout<<"Ingresa tamaño";
+			cin>>x;
+			logaritmo(x);
+		}
 	}while(n!=0);
 }
 
-	void seno(int lim, int x)
+	void seno(int x)
 	{
-		float sen=x;
-		int flag=3;
+		double sen=x , y=3.0;
 		
-		for(int i=2;i<=lim;i++)
+		for(int i=2;i<=99;i++)
 			{
 				if(i%2==0)
-					sen-=(pow(x,flag))/factorial(flag);
+					sen-=(pow(x,y))/factorial(y);
 				else
-					sen+=(pow(x,flag))/factorial(flag);
-			flag+=2;
+					sen+=(pow(x,y))/factorial(y);
+			y+=2.0;
 			}
 		cout<<"Seno("<<x<<")="<<sen;
 	}
 	
-		void coseno(int lim, int x)
+	void coseno(int x)
 	{
-		float cosen=1.0;
-		int flag=2;
+		double cosen=1.0 , y = 2.0;
 		
-		for(int i=2;i<=lim;i++)
+		for(int i=2;i<=99;i++)
 			{
 				if(i%2==0)
-					cosen-=(pow(x,flag))/factorial(flag);
+					cosen-=(pow(x,y))/factorial(y);
 				else
-					cosen+=(pow(x,flag))/factorial(flag);
-			flag+=2;
+					cosen+=(pow(x,y))/factorial(y);
+			y+=2;
 			}
 		cout<<"Coseno("<<x<<")="<<cosen;
 	}
 
-	void logaritmo(int lim, int x)
+	void logaritmo(int x)
 	{
-		float constante=((x-1.0)/x);
-		float ln=constante;
-		
-		for(int i=2;i<=lim;i++)
+		double constante=((x-1.0)/x);
+		double ln=constante;
+
+		for(int i=2;i<=99;i++)
 			ln+=(1.0/i)*(pow(constante,i));
 		cout<<"Ln("<<x<<")="<<ln;
 	}
 
-	
-	int factorial(int n)
+	float factorial(float n)
 	{
-		int fac=1;
-		
+		float fac=1.0;
 		for(int i=2;i<=n;i++)
 			fac*=i;
 		return fac;
 	}
+void menu(){
+	cout<<"1. Seno\n";
+	cout<<"2. Coseno\n";
+	cout<<"3. Logaritmo Natural\n";
+	cout<<"0. Salir\n";
+	cout<<"Opcion:";
+}
